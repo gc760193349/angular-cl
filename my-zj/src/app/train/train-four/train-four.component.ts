cf2287089cf3118dccb12e39f7002727fcdd9588
gc760193349 <gc760193349@qq.com>
2018-12-17 10:43:00 +0800

    fix-two

--- my-zj/src/app/train/train-four/train-four.component.ts
+++ my-zj/src/app/train/train-four/train-four.component.ts
@@ -18,22 +18,22 @@ export class TrainFourComponent implements OnInit {
   }
 
   getMap(): void {
-    let map = new AMap.Map('container', {
+    let map: any = new AMap.Map('container', {
       resizeEnable: true,
       zoom: 30,
       center: [123.44778, 41.72246],
       isHotspot: true
     });
 
-    AMapUI.loadUI(['overlay/SimpleInfoWindow'], function (SimpleInfoWindow) {
+    AMapUI.loadUI(['overlay/SimpleInfoWindow'], (SimpleInfoWindow) => {
 
-      let marker = new AMap.Marker({
+      let marker: any = new AMap.Marker({
         map: map,
         zIndex: 9999999,
         position: map.getCenter()
       });
 
-      let infoWindow = new SimpleInfoWindow({
+      let infoWindow: any = new SimpleInfoWindow({
 
         infoTitle: "信息",
         infoBody: '姓名:<%- name %> </br>  性别:<%- sex %> </br> 年龄:<%- age %> </br> 电话:<%- tel %> </br> 地址:<%- add %>',
@@ -49,44 +49,43 @@ export class TrainFourComponent implements OnInit {
         offset: new AMap.Pixel(0, -31)
       });
 
-      function openInfoWin(): void {
+      let openInfoWin = (): void => {
         infoWindow.open(map, marker.getPosition());
-      }
+      };
 
       //marker 点击时打开
-      AMap.event.addListener(marker, 'click', function (): void {
+      AMap.event.addListener(marker, 'click', (): void => {
         openInfoWin();
       });
 
       openInfoWin();
     });
-
     let placeSearch = new AMap.PlaceSearch();  //构造地点查询类
     let infoWindow = new AMap.AdvancedInfoWindow({});
-    map.on('hotspotclick', function (result: any): void {
-      placeSearch.getDetails(result.id, function (status: any, result: any): void {
+    map.on('hotspotclick',  (result) => {
+      placeSearch.getDetails(result.id, function (status, result) {
         if (status === 'complete' && result.info === 'OK') {
           placeSearch_CallBack(result);
         }
       });
-    });
 
-    //回调函数
-    function placeSearch_CallBack(data: any): void { //infoWindow.open(map, result.lnglat);
-      let poiArr = data.poiList.pois;
-      let location = poiArr[0].location;
-      infoWindow.setContent(createContent(poiArr[0]));
-      infoWindow.open(map, location);
-    }
 
-    function createContent(poi: any): string {  //信息窗体内容
-      let s = [];
-      s.push('<div class="info-title">' + poi.name + '</div><div class="info-content">' + "地址：" + poi.address);
-      s.push("电话：" + poi.tel);
-      s.push("类型：" + poi.type);
-      s.push('</div>');
-      return s.join("<br>");
-    }
+      //回调函数
+      let placeSearch_CallBack = (data: any): void => {
+        let poiArr: any = data.poiList.pois;
+        let location: any = poiArr[0].location;
+        infoWindow.setContent(createContent(poiArr[0]));
+        infoWindow.open(map, location);
+      };
 
+      let createContent = (poi: any): string => {  //信息窗体内容
+        let s: any = [];
+        s.push('<div class="info-title">' + poi.name + '</div><div class="info-content">' + "地址：" + poi.address);
+        s.push("电话：" + poi.tel);
+        s.push("类型：" + poi.type);
+        s.push('</div>');
+        return s.join("<br>");
+      }
+    })
   }
 }
